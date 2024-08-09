c6fe229c45495a837ab4aa5e340f62248ca43093
MoritzVolkman <moeserdkunde@gmail.com>
2024-08-09 15:29:13 +0200

    Added NS3 Sim that does something (not sure what yet) - but should be a good blueprint

--- NetSim.py
+++ NetSim.py
@@ -1,3 +1,5 @@
+import json
+
 try:
     from ns import ns
 except ModuleNotFoundError:
@@ -7,44 +9,68 @@ except ModuleNotFoundError:
         " or your PYTHONPATH might not be properly configured"
     )
 
-# At the moment, this is just the example from the ns-3 tutorial, will be adapted to our purposes
+cmd = ns.CommandLine(__file__)
 
-ns.LogComponentEnable("UdpEchoClientApplication", ns.LOG_LEVEL_INFO)
-ns.LogComponentEnable("UdpEchoServerApplication", ns.LOG_LEVEL_INFO)
-ns.LogComponentEnable("TcpRxBuffer", ns.LOG_LEVEL_INFO)
 
-nodes = ns.NodeContainer()
-nodes.Create(2)
+def simulate_SGMW_TAF14():
+    # Set Data Rate and Packet Size for the OnOffApplication
+    ns.Config.SetDefault("ns3::OnOffApplication::PacketSize", ns.StringValue("1024"))
+    ns.Config.SetDefault("ns3::OnOffApplication::DataRate", ns.StringValue("5Mb/s"))
 
-pointToPoint = ns.PointToPointHelper()
-pointToPoint.SetDeviceAttribute("DataRate", ns.StringValue("5Mbps"))
-pointToPoint.SetChannelAttribute("Delay", ns.StringValue("2ms"))
+    # Create the prosumer nodes
+    prosumer_nodes = ns.NodeContainer()
+    prosumer_nodes.Create(2)
 
-devices = pointToPoint.Install(nodes)
+    # Create the grid operator node
+    grid_operator_node = ns.NodeContainer()
+    grid_operator_node.Create(1)
 
-stack = ns.InternetStackHelper()
-stack.Install(nodes)
+    point_to_point = ns.PointToPointHelper()
+    point_to_point.SetDeviceAttribute("DataRate", ns.StringValue("5Mbps"))
+    point_to_point.SetChannelAttribute("Delay", ns.StringValue("2ms"))
 
-address = ns.Ipv4AddressHelper()
-address.SetBase(ns.Ipv4Address("10.1.1.0"), ns.Ipv4Mask("255.255.255.0"))
+    devices = []
 
-interfaces = address.Assign(devices)
+    # Create the point-to-point link between the prosumer and the grid operator
+    for i in range(0, prosumer_nodes.GetN()):
+        device = point_to_point.Install(prosumer_nodes.Get(i), grid_operator_node.Get(0))
+        devices.append(device)
 
-echoServer = ns.UdpEchoServerHelper(9)
+    # Install the internet stack on the prosumer and grid operator nodes
+    stack = ns.InternetStackHelper()
+    stack.Install(prosumer_nodes)
+    stack.Install(grid_operator_node)
 
-serverApps = echoServer.Install(nodes.Get(1))
-serverApps.Start(ns.Seconds(1.0))
-serverApps.Stop(ns.Seconds(10.0))
+    address = ns.Ipv4AddressHelper()
+    address.SetBase(ns.Ipv4Address("10.1.1.0"), ns.Ipv4Mask("255.255.255.0"))
 
-address = interfaces.GetAddress(1).ConvertTo()
-echoClient = ns.UdpEchoClientHelper(address, 9)
-echoClient.SetAttribute("MaxPackets", ns.UintegerValue(1))
-echoClient.SetAttribute("Interval", ns.TimeValue(ns.Seconds(1.0)))
-echoClient.SetAttribute("PacketSize", ns.UintegerValue(1024))
+    # Assign IP addresses to the prosumer and grid operator nodes
+    for i in range(len(devices)):
+        interfaces = address.Assign(devices[i])
 
-clientApps = echoClient.Install(nodes.Get(0))
-clientApps.Start(ns.Seconds(2.0))
-clientApps.Stop(ns.Seconds(10.0))
+    # Create the Server Application on the grid operator node
+    port = 8080
+    print(type(ns.InetSocketAddress(ns.Ipv4Address.GetAny(), port).ConvertTo()))
+    server_helper = ns.PacketSinkHelper("ns3::TcpSocketFactory", ns.InetSocketAddress(ns.Ipv4Address.GetAny(), port).ConvertTo())
+    server_apps = server_helper.Install(grid_operator_node.Get(0))
+    server_apps.Start(ns.Seconds(1.0))
+    server_apps.Stop(ns.Seconds(10.0))
 
-ns.Simulator.Run()
-ns.Simulator.Destroy()
+    # Create a TCP client on the first prosumer node
+    client_helper = ns.BulkSendHelper("ns3::TcpSocketFactory", ns.InetSocketAddress(interfaces.GetAddress(1), port).ConvertTo())
+    client_helper.SetAttribute("MaxBytes", ns.UintegerValue(1024))
+
+    client_apps = client_helper.Install(prosumer_nodes.Get(0))
+    client_apps.Start(ns.Seconds(2.0))
+    client_apps.Stop(ns.Seconds(10.0))
+
+    ns.Simulator.Run()
+    ns.Simulator.Destroy()
+
+
+if __name__ == "__main__":
+    simulate_SGMW_TAF14()
+
+
+
+
