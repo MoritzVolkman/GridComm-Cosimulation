f68a8a2c1ec1f8b30864efcc1685bc1cf4ca7312
acb017 <moeserdkunde@gmail.com>
2024-08-23 22:59:03 +0200

    Moved FDIA functions to FDIA.py

--- FDIA.py
+++ FDIA.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Injects false data into the measurement data for the specified busses
 # The mode specifies the type of false data that is injected
@@ -7,32 +9,80 @@ import numpy as np
 # Mode 2: Informed obstruction of the system - trying to make the system unstable with calculations
 
 def random_fdia(busses, measurements):
-        # Select the JSON object from the list where the "ConsumerID" matches the bus
-        # Add random values for ActivePower, ReactivePower and Voltage
-        # Random Reactive Power should be in the range 10e-05 to -10e-05
-        # Random Active Power should be in the range 10e-06 to 10e-03
-        # Voltage should be close to 1.0
-        # Voltage angle is not part of the state estimation, therefore it is not considered
-        for bus in busses:
-            for measurement in measurements:
-                if measurement["UserInformation"]["ConsumerID"] == bus:
-                    measurement["MeasurementData"]["ActivePower"] = np.random.uniform(10e-06, 10e-02)
-                    measurement["MeasurementData"]["ReactivePower"] = np.random.uniform(-10e-05, 10e-05)
-                    measurement["MeasurementData"]["Voltage"] = np.random.uniform(0.95, 1.05)
+    # Select the JSON object from the list where the "ConsumerID" matches the bus
+    # Add random values for ActivePower, ReactivePower and Voltage
+    # Random Reactive Power should be in the range 10e-05 to -10e-05
+    # Random Active Power should be in the range 10e-06 to 10e-03
+    # Voltage should be close to 1.0
+    # Voltage angle is not part of the state estimation, therefore it is not considered
+    for bus in busses:
+        for measurement in measurements:
+            if measurement["UserInformation"]["ConsumerID"] == bus:
+                measurement["MeasurementData"]["ActivePower"] = np.random.uniform(10e-06, 10e-02)
+                measurement["MeasurementData"]["ReactivePower"] = np.random.uniform(-10e-05, 10e-05)
+                measurement["MeasurementData"]["Voltage"] = np.random.uniform(0.95, 1.05)
 
 
 def uninformed_fdia(busses, measurements):
-        # Select the JSON object from the list where the "ConsumerID" matches the bus
-        # Add values to the ActivePower, ReactivePower and Voltage that try to make the system unstable
-        # ActivePower and ReactivePower should be high, Voltage should be low
-        # The FDIA should try to bypass the bad data detection
-        for bus in busses:
-            for measurement in measurements:
-                if measurement["UserInformation"]["ConsumerID"] == bus:
-                    measurement["MeasurementData"]["ActivePower"] = 0.3
-                    measurement["MeasurementData"]["ReactivePower"] = -0.005
-                    measurement["MeasurementData"]["Voltage"] = 0.999
+    # Select the JSON object from the list where the "ConsumerID" matches the bus
+    # Add values to the ActivePower, ReactivePower and Voltage that try to make the system unstable
+    # ActivePower and ReactivePower should be high, Voltage should be low
+    # The FDIA should try to bypass the bad data detection
+    for bus in busses:
+        for measurement in measurements:
+            if measurement["UserInformation"]["ConsumerID"] == bus:
+                measurement["MeasurementData"]["ActivePower"] = 0.3
+                measurement["MeasurementData"]["ReactivePower"] = -0.005
+                measurement["MeasurementData"]["Voltage"] = 0.999
 
 
 def informed_fdia(busses, measurements):
     pass
+
+
+def plot_differences(correct_data, fdia_data):
+    # Plot the differences between the correct and the FDIA data
+    # The differences are calculated as the difference between the correct and the FDIA data
+    # The differences are then plotted for each bus
+    differences = compute_differences(correct_data, fdia_data)
+    print("Average Differences in %: ")
+    print(differences.mean())
+    differences.iloc[0:42].plot(subplots=True,xlabel="Bus Number", ylabel="Difference in %",
+                     title=["Voltage Difference", "Active Power Difference",
+                            "Reactive Power Difference", "Voltage Angle Difference"])
+    plt.show()
+    return differences
+
+
+def compute_differences(correct_data, fdia_data):
+    # Computes the percentage differences between the correct and the FDIA data and puts them in the differences dataframe
+    # The differences are calculated as (FDIA - Correct) / Correct * 100
+    differences = pd.DataFrame()
+    for column in correct_data.columns:
+        differences[f"d_{column}"] = ((fdia_data[column] - correct_data[column]) / correct_data[column]) * 100
+    return differences
+
+
+def plot_attack(net, attack_buses):
+    # Take the geodata of the attack_buses from net["bus_geodata"] and plot them as red dots
+    # Take the geodata of the rest of the buses from net["bus_geodata"] and plot them as blue dots
+    # Takes the start and end points of the lines "from_bus" and "to_bus" from net["line"] and plots them
+    bus_geodata = net["bus_geodata"]
+    attack_geodata = bus_geodata.loc[attack_buses]
+    rest_geodata = bus_geodata.drop(attack_buses)
+    line_geodata = net["line"]
+    for line in line_geodata.iterrows():
+        x0 = bus_geodata.loc[line[1]["from_bus"]]["x"]
+        y0 = bus_geodata.loc[line[1]["from_bus"]]["y"]
+        x1 = bus_geodata.loc[line[1]["to_bus"]]["x"]
+        y1 = bus_geodata.loc[line[1]["to_bus"]]["y"]
+        plt.plot([x0, x1], [y0, y1], color="black")
+    plt.scatter(rest_geodata["x"], rest_geodata["y"], color="blue")
+    plt.scatter(attack_geodata["x"], attack_geodata["y"], color="red")
+    """
+    # Way too small to read, maybe add a text box for each bus
+    # Add the measurement data for each bus to the plot such as (v = Voltage, p = Active Power, q = Reactive Power)
+    for bus in net.res_bus.index:
+        plt.text(bus_geodata.loc[bus]["x"], bus_geodata.loc[bus]["y"], f"v: {net.res_bus.loc[bus]['vm_pu']:.2f}\np: {net.res_bus.loc[bus]['p_mw']:.2f}\nq: {net.res_bus.loc[bus]['q_mvar']:.2f}")
+    """
+    plt.show()
--- GridSim.py
+++ GridSim.py
@@ -5,7 +5,6 @@ import pandapower
 import socket
 import os
 import math
-
 import pandas as pd
 import simbench as sb
 import FDIA as fdia
@@ -37,60 +36,15 @@ def main():
         # Conduct FDIA on the measurement data
         attack_buses = [40, 41, 42]
         fdia.random_fdia(attack_buses, SMGW_data)
-        plot_attack(net, attack_buses)
+        fdia.plot_attack(net, attack_buses)
         # send_to_network_sim(SMGW_data, i)
         # parse the measurement data from the network simulator SMGW_data will be replaced by GO_data
         parse_measurement(SMGW_data, net) #replace SMGW_data with GO_data to incorporate network sim
         run_state_estimation(net)
-        plot_differences(correct_data, net.res_bus_est)
+        fdia.plot_differences(correct_data, net.res_bus_est)
 
 
-def plot_differences(correct_data, fdia_data):
-    # Plot the differences between the correct and the FDIA data
-    # The differences are calculated as the difference between the correct and the FDIA data
-    # The differences are then plotted for each bus
-    differences = compute_differences(correct_data, fdia_data)
-    print("Average Differences in %: ")
-    print(differences.mean())
-    differences.iloc[0:42].plot(subplots=True,xlabel="Bus Number", ylabel="Difference in %",
-                     title=["Voltage Difference", "Active Power Difference", "Reactive Power Difference", "Voltage Angle Difference"])
-    plt.show()
-    return differences
 
-
-def compute_differences(correct_data, fdia_data):
-    # Computes the percentage differences between the correct and the FDIA data and puts them in the differences dataframe
-    # The differences are calculated as (FDIA - Correct) / Correct * 100
-
-    differences = pd.DataFrame()
-    for column in correct_data.columns:
-        differences[f"d_{column}"] = ((fdia_data[column] - correct_data[column]) / correct_data[column]) * 100
-    return differences
-
-
-def plot_attack(net, attack_buses):
-    # Take the geodata of the attack_buses from net["bus_geodata"] and plot them as red dots
-    # Take the geodata of the rest of the buses from net["bus_geodata"] and plot them as blue dots
-    # Takes the start and end points of the lines "from_bus" and "to_bus" from net["line"] and plots them
-    bus_geodata = net["bus_geodata"]
-    attack_geodata = bus_geodata.loc[attack_buses]
-    rest_geodata = bus_geodata.drop(attack_buses)
-    line_geodata = net["line"]
-    for line in line_geodata.iterrows():
-        x0 = bus_geodata.loc[line[1]["from_bus"]]["x"]
-        y0 = bus_geodata.loc[line[1]["from_bus"]]["y"]
-        x1 = bus_geodata.loc[line[1]["to_bus"]]["x"]
-        y1 = bus_geodata.loc[line[1]["to_bus"]]["y"]
-        plt.plot([x0, x1], [y0, y1], color="black")
-    plt.scatter(rest_geodata["x"], rest_geodata["y"], color="blue")
-    plt.scatter(attack_geodata["x"], attack_geodata["y"], color="red")
-    """
-    # Way too small to read, maybe add a text box for each bus
-    # Add the measurement data for each bus to the plot such as (v = Voltage, p = Active Power, q = Reactive Power)
-    for bus in net.res_bus.index:
-        plt.text(bus_geodata.loc[bus]["x"], bus_geodata.loc[bus]["y"], f"v: {net.res_bus.loc[bus]['vm_pu']:.2f}\np: {net.res_bus.loc[bus]['p_mw']:.2f}\nq: {net.res_bus.loc[bus]['q_mvar']:.2f}")
-    """
-    plt.show()
 
 
 def run_state_estimation(net):
@@ -234,7 +188,7 @@ def train_fdia():
             fdia.random_fdia([i], SMGW_data)
             parse_measurement(SMGW_data, net)
             run_state_estimation(net)
-            differences = compute_differences(net.res_bus_est, correct_data).mean().transpose()
+            differences = fdia.compute_differences(net.res_bus_est, correct_data).mean().transpose()
             # Add the mean of the differences to the effect dataframe
             effect = pd.concat([effect, differences], ignore_index=True, axis=1)
         # Plot the effect of the FDIA attack on the grid
@@ -247,14 +201,14 @@ def train_fdia():
         effect.plot(subplots=True, xlabel="Bus Number", ylabel="Difference in %",
                     title=["Voltage Difference", "Active Power Difference", "Reactive Power Difference", "Voltage Angle Difference"])
         plt.show()
-        plot_attack(net, [effect['d_vm_pu'].idxmax(), effect['d_va_degree'].idxmax(), effect['d_p_mw'].idxmax(),
+        fdia.plot_attack(net, [effect['d_vm_pu'].idxmax(), effect['d_va_degree'].idxmax(), effect['d_p_mw'].idxmax(),
                           effect['d_q_mvar'].idxmax()])"""
         # Add the most effective bus to the counter
         print(j)
         counter.loc[j] = effect.idxmax().values
-    # Print the most effective bus for each value
+    # Print the most effective bus for each value -> (Voltage: 42, Voltage Angle: 32, Power(both): 0)
     print(counter.mode())
-    plot_attack(net, counter.mode().iloc[0])
+    fdia.plot_attack(net, counter.mode().iloc[0])
 
 
 if __name__ == "__main__":
