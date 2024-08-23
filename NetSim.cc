c92c835df0c27b4416632a4f1fd96802822bad00
MoritzVolkman <moeserdkunde@gmail.com>
2024-08-23 13:27:01 +0200

    Integration of new Network functions

--- NetSim.cc
+++ NetSim.cc
@@ -5,9 +5,12 @@
 #include "ns3/applications-module.h"
 #include "ns3/pcap-file-wrapper.h"
 
+#include <nlohmann/json.hpp>
+
+#include "Network.hpp"
+
 #include <fstream>
 #include <sstream>
-#include <nlohmann/json.hpp>
 
 using namespace ns3;
 using json = nlohmann::json;
@@ -83,9 +86,8 @@ void MyApp::StopApplication(void)
 {
     m_running = false;
 
-    if (m_sendEvent.IsRunning())
-    {
-        Simulator::Cancel(m_sendEvent);
+    if (m_sendEvent.IsPending()) {
+      Simulator::Cancel(m_sendEvent);
     }
 
     if (m_socket)
@@ -173,6 +175,17 @@ int main(int argc, char* argv[])
 {
     LogComponentEnable("TcpExample", LOG_LEVEL_INFO);
 
+    auto message = network::wait_for_message(10020);
+    std::string msg_str = std::string{message.begin(), message.end()};
+    std::cout << msg_str << std::endl;
+
+    // wait for a second to get the python script into receiving mode
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    std::string response = "And Howdie to you, my friend!";
+    network::send_message("127.0.0.1", 10021, response);
+
+    return 0;
     std::string jsonData[9];
     MyApp appInstance;
     appInstance.LoadJsonData("../../../../../PycharmProjects/GridComm-Cosimulation/JSON/", jsonData, 9);
