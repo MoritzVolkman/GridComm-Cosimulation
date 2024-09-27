4a1237168d1376f9621e31e456e546fe09ee7295
MoritzVolkmann <moeserdkunde@gmail.com>
2024-09-27 14:11:18 +0200

    Added results

--- GridSim.py
+++ GridSim.py
@@ -204,6 +204,7 @@ def run_sim_with_stateest_for_powerflow():
     grid_health = 0
     all_differences = []
     num_timesteps = 96
+    model, bounds = fdia.deep_learning_fdia_train_model()
     for i in range(num_timesteps):
         net.trafo.tap_pos = 1
         if i:
@@ -233,7 +234,10 @@ def run_sim_with_stateest_for_powerflow():
             grid_health += check_grid_health(net)
             # attack_data = fdia.random_fdia([0, 1, 2, 8, 9, 40], SMGW_data)
             # attack_data = fdia.random_fdia_liu([0, 1, 2, 8, 9, 40], SMGW_data, net, H)
-            attack_data = fdia.random_generalized_fdia_liu([0, 1, 2, 8, 9, 40], SMGW_data, net, H)
+            # attack_data = fdia.random_generalized_fdia_liu([0, 1, 2, 8, 9, 40], SMGW_data, net, H)
+            # attack_data = fdia.targeted_generalized_fdia_liu([0, 1, 2, 8, 9, 40], SMGW_data, net, H)
+            attack_vector = fdia.deep_learning_fdia_predict(model, bounds)
+            attack_data = fdia.deep_learning_fdia_inject(attack_vector, [0, 1, 2, 8, 9, 40], SMGW_data)
             parse_measurement(attack_data, net)
             liu_counter += 1
         run_state_estimation(net)
